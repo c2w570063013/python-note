2a45e68b11b756df3c2e754379cc93ce73c695bc
wayne <570063013@qq.com>
2019-11-11 11:34:00 +0800

    new aav scrawler

---
+++ crawler/aavc/test.py
@@ -0,0 +1,15 @@
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Cache-Control": "max-age=0",
+    "Connection": "keep-alive",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Host": "exhibitors.electronica.de",
+    "Origin": "https://exhibitors.electronica.de",
+    "Referer": "https://exhibitors.electronica.de/onlinecatalog/2018/Search_result/",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
+}
+
+
+def scrape():
+    pass
